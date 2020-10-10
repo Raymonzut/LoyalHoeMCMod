51f4035b65b95fb3dc4b005865d07710f02a762c
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2020-10-10 22:58:38 +0200

    Allow enchanting a hoe with loyalty

--- src/main/java/dev/raymon/loyalhoe/LoyalHoeMod.java
+++ src/main/java/dev/raymon/loyalhoe/LoyalHoeMod.java
@@ -1,9 +1,13 @@
 package dev.raymon.loyalhoe;
 
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.HoeItem;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,9 +23,15 @@ public class LoyalHoeMod
     public LoyalHoeMod()
     {
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);
 
         instance = this;
 
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        Enchantments.LOYALTY.type = EnchantmentType.create("Hoe", item -> item instanceof HoeItem);
     }
 }
