6f179c402e8137a1c393d3a9b82002c4b4590e38
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2020-10-10 17:59:54 +0200

    Restrict effect to non-loyal hoe

--- src/main/java/dev/raymon/loyalhoe/events/HoeTossedEvent.java
+++ src/main/java/dev/raymon/loyalhoe/events/HoeTossedEvent.java
@@ -1,12 +1,16 @@
 package dev.raymon.loyalhoe.events;
 
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.item.ItemEntity;
-import net.minecraft.item.HoeItem;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+import java.util.Map;
 
 import dev.raymon.loyalhoe.LoyalHoeMod;
 
@@ -18,11 +22,17 @@ public class HoeTossedEvent
     {
         ItemEntity itemEntity = event.getEntityItem();
         Item item = itemEntity.getItem().getItem();
-        if (item instanceof HoeItem)
-        {
-            LoyalHoeMod.LOGGER.info("Tossed a hoe");
-            itemEntity.addVelocity(0, 1.25, 0);
-            itemEntity.setInfinitePickupDelay();
+
+        if (item instanceof HoeItem){
+            ItemStack itemStack = itemEntity.getItem();
+            Map<Enchantment, Integer> enchantmentPairs = EnchantmentHelper.getEnchantments(itemStack);
+
+            boolean hasLoyalty = enchantmentPairs.containsKey(Enchantments.LOYALTY);
+            if (!hasLoyalty) {
+                LoyalHoeMod.LOGGER.info("Tossed non-loyal hoe");
+                itemEntity.addVelocity(0, 1.25, 0);
+                itemEntity.setInfinitePickupDelay();
+            }
         }
     }
 }
